afbcd384fd16219605f687282612b96b280edb21
YuraVasko <yuravasko2016@gmail.com>
2019-03-22 01:17:38 +0200

    Add menuItemModel

---
+++ FoodDelivery/FoodDelivery/Models/MenuItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDelivery.Models
+{
+    public class MenuItemModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
